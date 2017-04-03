8670c0bc1e613f177f4abf451987809d11baf0e4
Nicolas Oga <2112.oga@gmail.com>
2017-04-02 21:00:18 -0300

    Change for prerender: accept prerender reuests and redirect to configurable prerender host

--- initialize.sh
+++ initialize.sh
@@ -2,21 +2,25 @@
 cat /tmp/nginx.conf.template \
   | \
     envsubst '\
-    \$API_DOMAIN \
-    \$API_SOCK_PATH \
-    \$API_ASSETS_PATH \
-
+    \$ADMIN_ASSETS_PATH \
     \$ADMIN_DOMAIN \
     \$ADMIN_SOCK_PATH \
-    \$ADMIN_ASSETS_PATH \
+
+    \$API_ASSETS_PATH \
+    \$API_DOMAIN \
+    \$API_SOCK_PATH \
+
+    \$ERRBIT_DOMAIN \
+    \$ERRBIT_HOST \
 
     \$FRONT_DOMAIN \
 
-    \$SOCK_FILE \
-    \$ERRBIT_DOMAIN \
-    \$ERRBIT_HOST \
     \$PGADMIN_DOMAIN \
     \$PGADMIN_HOST \
+
+    \$SOCK_FILE \
+
+    \$PRERENDERHOST \
   ' \
   > /etc/nginx/nginx.conf
 
