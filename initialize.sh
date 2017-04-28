47072c60e8a5e4c5948cc596c3202d0dd22ae76a
Nicolas Oga <2112.oga@gmail.com>
2017-04-28 18:10:44 -0300

    Changes

--- initialize.sh
+++ initialize.sh
@@ -33,7 +33,6 @@ for f in $(find /tmp/nginx_conf | grep "\.template$"); do
 
       \$SSL_CERTIFICATE     \
       \$SSL_CERTIFICATE_KEY \
-
     ' \
     > ${f%.template}
 
@@ -41,7 +40,7 @@ for f in $(find /tmp/nginx_conf | grep "\.template$"); do
 done
 
 rsync -ra /tmp/nginx_conf/* /etc/nginx/
-rsync -ra /tmp/certs $SSL_DIRS_PATH
+rsync -ra /tmp/certs/* $SSL_CERTS_PATH/
 
 echo "This is what /etc/nginx/nginx.conf looks like:"
 cat /etc/nginx/nginx.conf
